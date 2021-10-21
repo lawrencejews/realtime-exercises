570eb1e11005192e13a2714c6f1ee965251fe0c5
lawrencejews <lawrencejews1@gmail.com>
2021-10-21 10:49:17 +0900

    http2 && local certificates

--- http2/exercise/backend/server.js
+++ http2/exercise/backend/server.js
@@ -18,10 +18,12 @@ msg.push({
 
 // the two commands you'll have to run in the root directory of the project are
 // (not inside the backend folder)
-//
+
+// GENERATE KEYS WITH OPENSSL
 // openssl req -new -newkey rsa:2048 -new -nodes -keyout key.pem -out csr.pem
 // openssl x509 -req -days 365 -in csr.pem -signkey key.pem -out server.crt
-//
+
+
 // http2 only works over HTTPS
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 const server = http2.createSecureServer({
@@ -29,11 +31,28 @@ const server = http2.createSecureServer({
   key: fs.readFileSync(path.join(__dirname, "/../key.pem")),
 });
 
-/*
- *
- * Code goes here
- *
- */
+server.on("stream", (stream, headers) => {
+  const path = headers[":path"];
+  const method = headers[":method"];
+
+  //streams open for every request from the browser
+  if (path === '/msgs' && method === 'GET') {
+    console.log('connected a stream' + stream.id);
+    stream.respond({
+      ":status": 200,
+      "content-type": "text/plain; charat=utf-8"
+    })
+
+    //write the first response
+    stream.write(JSON.stringify({ msg: getMsgs() }))
+    connections.push(stream);
+    
+    stream.on('close', () => {
+      console.log("disconnected", + stream.id);
+      connections = connections.filter(s => s !== stream);
+    })
+  }
+})
 
 server.on("request", async (req, res) => {
   const path = req.headers[":path"];
@@ -53,11 +72,16 @@ server.on("request", async (req, res) => {
     const data = Buffer.concat(buffers).toString();
     const { user, text } = JSON.parse(data);
 
-    /*
-     *
-     * some code goes here
-     *
-     */
+    msg.push({
+      user,
+      text,
+      time: Date.nnow()
+    });
+    res.end()
+
+    connections.forEach((stream) => {
+      stream.write(JSON.stringify({ msg: getMsgs() }));
+    })
   }
 });
 
